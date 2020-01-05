35b8d1979c335f2b31a37e65163d06f51c344369
Anthony Abate <anthony.abate@gmail.com>
2020-01-05 16:11:09 -0500

    remove files

--- Build/Common/Powershell/integrationtest.ps1
+++
@@ -1,5 +0,0 @@
-. $PSScriptRoot/test.ps1 "integrationtest"
-
-echo "Exit integrationtest.ps1 with $EXIT_CODE"
-
-Exit $EXIT_CODE
--- Build/Common/Powershell/unittest.ps1
+++
@@ -1,5 +0,0 @@
-. $PSScriptRoot/test.ps1 "unittest"
-
-echo "Exit unittest.ps1 with $EXIT_CODE"
-
-Exit $EXIT_CODE
