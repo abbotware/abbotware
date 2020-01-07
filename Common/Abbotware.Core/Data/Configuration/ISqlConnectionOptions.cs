7f2ac84aa07b54f923a9f39f670402fd1916075e
Anthony Abate <anthony.abate@gmail.com>
2020-01-06 22:50:22 -0500

    add missing property for ConnectionString

--- Common/Abbotware.Core/Data/Configuration/ISqlConnectionOptions.cs
+++ Common/Abbotware.Core/Data/Configuration/ISqlConnectionOptions.cs
@@ -17,22 +17,27 @@ namespace Abbotware.Core.Data.Configuration
         /// <summary>
         ///     Gets the timeout for bulk operations
         /// </summary>
-        TimeSpan BulkCommandTimeout { get;  }
+        TimeSpan BulkCommandTimeout { get; }
 
         /// <summary>
         ///     Gets the timeout for an SQL Command
         /// </summary>
-        TimeSpan CommandTimeout { get;  }
+        TimeSpan CommandTimeout { get; }
 
         /// <summary>
         ///     Gets the timeout for starting a new connection to the SQL Server
         /// </summary>
-        TimeSpan ConnectionTimeout { get;  }
+        TimeSpan ConnectionTimeout { get; }
 
         /// <summary>
         ///     Gets the isolation level for the connection
         /// </summary>
-        IsolationLevel IsolationLevel { get;  }
+        IsolationLevel IsolationLevel { get; }
+
+        /// <summary>
+        ///     Gets the Connection extracted from app config file or supplied via constructor
+        /// </summary>
+        string ConnectionString { get; }
 
         /// <summary>
         ///     Gets the full connection string or 'name={enties}' identifying the name of the connection string in the app.config
@@ -47,7 +52,7 @@ namespace Abbotware.Core.Data.Configuration
         /// <summary>
         ///     Gets the timeout for a transaction
         /// </summary>
-        TimeSpan TransactionTimeout { get;  }
+        TimeSpan TransactionTimeout { get; }
 
         /// <summary>
         ///     Gets a value indicating whether or not schema validation should be performed
