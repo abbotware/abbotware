27bb615f8f3cd88d60b9d9c5d2f081e0d7c8146c
Anthony Abate <anthony.abate@gmail.com>
2020-01-10 21:24:31 -0500

    bi directional converter interface

---
+++ Common/Abbotware.Core/IBidirectionalConverter{TFrom,TTo}.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IBidirectionalConverter{TFrom,TTo}.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Core
+{
+    /// <summary>
+    /// Interface for bi directional converting
+    /// </summary>
+    /// <typeparam name="TFrom">From Type</typeparam>
+    /// <typeparam name="TTo">To Typ</typeparam>
+    public interface IBidirectionalConverter<TFrom, TTo>
+    {
+        /// <summary>
+        /// Gets the From -> To Converter
+        /// </summary>
+        IConverter<TFrom, TTo> Forward { get; }
+
+        /// <summary>
+        /// Gets the To -> From Converter
+        /// </summary>
+        IConverter<TTo, TFrom> Reverse { get; }
+    }
+}
