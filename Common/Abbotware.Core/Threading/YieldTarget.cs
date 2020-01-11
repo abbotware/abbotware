9f199cb80da6eaddc8cf8f97b9d002f924b7a107
Anthony Abate <anthony.abate@gmail.com>
2020-01-11 02:06:32 -0500

    moved code to github

---
+++ Common/Abbotware.Core/Threading/YieldTarget.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="YieldTarget.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Anthony Abate</author>
+
+namespace Abbotware.Core.Threading
+{
+    /// <summary>
+    ///     Yield Type
+    /// </summary>
+    public enum YieldTarget
+    {
+        /// <summary>
+        ///     Operating system will decide when to interrupt the thread
+        /// </summary>
+        None,
+
+        /// <summary>
+        ///     Yield time slice to any other thread on any processor
+        /// </summary>
+        AnyThreadOnAnyProcessor,
+
+        /// <summary>
+        ///     Yield time slice to other thread of same or higher piority on any processor
+        /// </summary>
+        SameOrHigherPriorityThreadOnAnyProcessor,
+
+        /// <summary>
+        ///     Yield time slice to any other thread on same processor
+        /// </summary>
+        AnyThreadOnSameProcessor,
+    }
+}
