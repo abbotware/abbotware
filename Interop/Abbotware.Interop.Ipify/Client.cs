e7cdc12b0f455865262c4a0115b4f5da988c67bc
Anthony Abate <anthony.abate@gmail.com>
2020-01-17 21:14:51 -0500

    moved ipify client to github

---
+++ Interop/Abbotware.Interop.Ipify/Client.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Client.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Anthony Abate</author>
+
+namespace Abbotware.Interop.Ipify
+{
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Abbotware.Core.Logging;
+    using Abbotware.Core.Net;
+    using Abbotware.Interop.Ipify.Configuration;
+    using Abbotware.Web.Api.Configuration.Models;
+    using Abbotware.Web.Objects;
+
+    /// <summary>
+    /// Ipify Client
+    /// </summary>
+    public class Client : WebComponent<ApiClientOptions>, IGetInternetAddress
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Client"/> class.
+        /// </summary>
+        /// <param name="logger">injected logger</param>
+        public Client(ILogger logger)
+            : base(new ApiClientOptions(Defaults.Endpoint), logger)
+        {
+        }
+
+        /// <inheritdoc/>
+        public async Task<IPAddress> GetAsync(CancellationToken ct)
+        {
+            this.InitializeIfRequired();
+
+            var data = await this.Client.GetAsync<string>(this.Configuration.BaseUri, ct)
+                .ConfigureAwait(false);
+
+            return IPAddress.Parse(data);
+        }
+    }
+}
---
+++ Interop/Abbotware.Interop.Ipify/Configuration/Defaults.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Defaults.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.Ipify.Configuration
+{
+    using System;
+
+    /// <summary>
+    /// Configuration defaults
+    /// </summary>
+    public static class Defaults
+    {
+        /// <summary>
+        /// Gets the base api endpoint
+        /// </summary>
+        public static Uri Endpoint { get; } = new Uri("https://api.ipify.org");
+    }
+}
