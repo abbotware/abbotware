d40e5db7e02ac11e705610be7dfadc03f4298e53
Anthony Abate <anthony.abate@gmail.com>
2020-01-17 22:28:19 -0500

    remove unnecessary dependencies

--- Test/Abbotware.Core.UnitTests/Threading/AtomicCounterTests.cs
+++ Test/Abbotware.Core.UnitTests/Threading/AtomicCounterTests.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright file="AtomicCounterTests.cs" company="Abbotware, LLC">
-// Copyright © Abbotware, LLC 2012-2016. All rights reserved
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
 // </copyright>
 
 namespace Abbotware.UnitTests.Core
---
+++ Test/Abbotware.Core.UnitTests/Threading/InstanceCounterTests.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="InstanceCounterTests.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+
+namespace Abbotware.UnitTests.Core
+{
+    using Abbotware.Core.Logging;
+    using Abbotware.Core.Objects;
+    using Abbotware.Core.Threading.Counters;
+    using Abbotware.Utility.UnitTest.Using.NUnit;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Category("Core")]
+    [Category("Core.Threading")]
+    public class InstanceCounterTests : BaseNUnitTest
+    {
+        [Test]
+        public void InstanceCounter_TestUsage()
+        {
+            Assert.AreEqual(0, TypeCreatedCounter<AClass>.Count);
+            Assert.AreEqual(0, ActiveInstanceCounter<AClass>.GlobalActiveCount);
+
+            using (var i1 = new AClass(this.Logger))
+            {
+                Assert.AreEqual(1, TypeCreatedCounter<AClass>.Count);
+                Assert.AreEqual(1, ActiveInstanceCounter<AClass>.GlobalActiveCount);
+
+                Assert.AreEqual(i1.InstanceId, 0);
+
+                using (var i2 = new AClass(this.Logger))
+                using (var i3 = new AClass(this.Logger))
+                {
+                    Assert.AreEqual(1, i2.InstanceId);
+
+                    Assert.AreEqual(3, TypeCreatedCounter<AClass>.Count);
+                    Assert.AreEqual(3, ActiveInstanceCounter<AClass>.GlobalActiveCount);
+
+                    using (var tracker = new ActiveInstanceCounter<AClass>(this.Logger))
+                    {
+                        Assert.AreEqual(4, ActiveInstanceCounter<AClass>.GlobalActiveCount);
+                        Assert.AreEqual(4, TypeCreatedCounter<AClass>.Count);
+                        Assert.AreEqual(4, tracker.ActiveCount);
+                    }
+
+                    Assert.AreEqual(3, ActiveInstanceCounter<AClass>.GlobalActiveCount);
+                    Assert.AreEqual(4, TypeCreatedCounter<AClass>.Count);
+                }
+
+                Assert.AreEqual(4, TypeCreatedCounter<AClass>.Count);
+                Assert.AreEqual(1, ActiveInstanceCounter<AClass>.GlobalActiveCount);
+            }
+
+            Assert.AreEqual(4, TypeCreatedCounter<AClass>.Count);
+            Assert.AreEqual(0, ActiveInstanceCounter<AClass>.GlobalActiveCount);
+        }
+
+        internal class AClass : BaseComponent
+        {
+            private readonly ActiveInstanceCounter<AClass> counter;
+
+            public AClass(ILogger logger)
+                : base(logger)
+            {
+                this.counter = new ActiveInstanceCounter<AClass>(logger);
+            }
+
+            public long InstanceId => this.counter.InstanceId;
+
+            protected override void OnDisposeManagedResources()
+            {
+                this.counter.Dispose();
+                base.OnDisposeManagedResources();
+            }
+        }
+    }
+}
