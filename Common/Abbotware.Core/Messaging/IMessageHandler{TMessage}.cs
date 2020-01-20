e0ed2d44f00430982249e560cf0faaa12ee62f67
Anthony Abate <anthony.abate@gmail.com>
2020-01-19 19:48:54 -0500

    start moving messaging classes to github

---
+++ Common/Abbotware.Core/Messaging/IMessageHandler{TMessage}.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IMessageHandler{TMessage}.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Anthony Abate</author>
+
+namespace Abbotware.Core.Messaging
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Basic message handler
+    /// </summary>
+    /// <typeparam name="TMessage">message type</typeparam>
+    public interface IMessageHandler<TMessage>
+    {
+        /// <summary>
+        /// Handles a message
+        /// </summary>
+        /// <param name="message">message</param>
+        /// <param name="ct">cancellation token</param>
+        /// <returns>async task</returns>
+        Task HandleAsync(TMessage message, CancellationToken ct);
+    }
+}
