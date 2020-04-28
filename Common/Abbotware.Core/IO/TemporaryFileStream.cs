82cebfff50f3d8ad64e162187ba49c6e8f50f73f
Anthony Abate <anthony.abate@gmail.com>
2020-04-27 22:51:19 -0400

    moved some code to github

---
+++ Common/Abbotware.Core/IO/TemporaryFileStream.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TemporaryFileStream.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Anthony Abate</author>
+
+namespace Abbotware.Core.IO
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    ///     Temp file that gets deleted on closing
+    /// </summary>
+    public class TemporaryFileStream : FileStream
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TemporaryFileStream" /> class.
+        /// </summary>
+        public TemporaryFileStream()
+            : this(new Uri(Path.GetTempFileName()))
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TemporaryFileStream" /> class.
+        /// </summary>
+        /// <param name="filePath">path to use for creating the temp file</param>
+        public TemporaryFileStream(Uri filePath)
+            : base(
+                  Arguments.EnsureNotNull(filePath, nameof(filePath)).LocalPath,
+                  FileMode.OpenOrCreate,
+                  FileAccess.ReadWrite,
+                  FileShare.ReadWrite,
+                  1024,
+                  FileOptions.DeleteOnClose)
+        {
+        }
+    }
+}
---
+++ Test/Abbotware.Core.UnitTests/IO/TemporaryFileStreamTests.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="TemporaryFileStreamTests.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+
+namespace Abbotware.UnitTests.Core
+{
+    using System;
+    using System.IO;
+    using Abbotware.Core.IO;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Category("Core")]
+    [Category("Core.IO")]
+    public class TemporaryFileStreamTests
+    {
+        [Test]
+        public void TemporaryFileS_NoName()
+        {
+            var fname = string.Empty;
+            using (var k = new TemporaryFileStream())
+            {
+                fname = k.Name;
+                Assert.IsTrue(File.Exists(fname));
+            }
+
+            Assert.IsFalse(File.Exists(fname));
+        }
+
+        [Test]
+        public void TemporaryFileS_WithName()
+        {
+            var filePath = Path.Combine(Path.GetTempPath(), "dummy.txt");
+
+            var fname = string.Empty;
+
+            using (var k = new TemporaryFileStream(new Uri(filePath)))
+            {
+                fname = k.Name;
+                Assert.IsTrue(File.Exists(fname));
+            }
+
+            Assert.IsFalse(File.Exists(fname));
+        }
+    }
+}
