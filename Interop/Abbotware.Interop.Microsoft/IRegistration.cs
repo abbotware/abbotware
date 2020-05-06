09c98d78e913e5c464c9ba26efda0953d6d6e044
Anthony Abate <anthony.abate@gmail.com>
2020-05-05 22:05:53 -0400

    added example service

---
+++ Interop/Abbotware.Interop.Microsoft/IRegistration.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IRegistration.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.Microsoft
+{
+    using global::Microsoft.Extensions.Configuration;
+    using global::Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Dependency registration interface (ASP Net Core / .Net Core)
+    /// </summary>
+    public interface IRegistration
+    {
+        /// <summary>
+        /// Configure / Register dependencies
+        /// </summary>
+        /// <param name="configuration">configuration</param>
+        /// <param name="services">services collection</param>
+        void Register(IConfiguration configuration, IServiceCollection services);
+    }
+}
---
+++ Interop/Abbotware.Interop.SystemServiceModel/ExampleClientFactoryUsage.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ExampleClientFactoryUsage.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.SystemServiceModel
+{
+    using System;
+    using System.ServiceModel;
+    using Example;
+
+    /// <summary>
+    /// Example Client Factory
+    /// </summary>
+    public class ExampleClientFactoryUsage : ISoapClientFactory<BLZServicePortTypeClient, BLZServicePortType>
+    {
+        /// <inheritdoc/>
+        public BLZServicePortTypeClient Create(Uri uri)
+        {
+            return this.Create(new EndpointAddress(uri));
+        }
+
+        /// <inheritdoc/>
+        public BLZServicePortTypeClient Create(EndpointAddress endpoint)
+        {
+            // set options if needed
+            var binding = new BasicHttpsBinding();
+
+            return new BLZServicePortTypeClient(binding, endpoint);
+        }
+    }
+}
