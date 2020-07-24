91337df1ee15925e0a926aa03756bb5661c6facf
Anthony Abate <anthony.abate@gmail.com>
2020-07-24 19:49:42 -0400

    enum helper for parsing

---
+++ Common/Abbotware.Core/Helpers/EnumHelper.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EnumHelper.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Anthony Abate</author>
+
+namespace Abbotware.Core.Helpers
+{
+    using System;
+
+    /// <summary>
+    ///     Helper methods related to enums
+    /// </summary>
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Parses a string into an enum value
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type</typeparam>
+        /// <param name="value">string version of enum</param>
+        /// <returns>enum value</returns>
+        public static TEnum ParseExact<TEnum>(string value)
+            where TEnum : struct, Enum
+        {
+            if (!Enum.TryParse(value, false, out TEnum parsed))
+            {
+                throw new ArgumentException($"{value} is not valid for enum:{typeof(TEnum).Name}");
+            }
+
+            return parsed;
+        }
+    }
+}
