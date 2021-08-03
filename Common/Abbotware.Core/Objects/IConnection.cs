60d5a28cebf6ecf797596f68378a3f629ee0a885
Anthony Abate <anthony.abate@gmail.com>
2021-08-02 23:27:49 -0400

    code analysis fixes

--- Common/Abbotware.Core/Objects/IConnection.cs
+++ Common/Abbotware.Core/Objects/IConnection.cs
@@ -1,6 +1,6 @@
-// -----------------------------------------------------------------------
+﻿// -----------------------------------------------------------------------
 // <copyright file="IConnection.cs" company="Abbotware, LLC">
-// Copyright � Abbotware, LLC 2012-2020. All rights reserved
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
 
--- Common/Abbotware.Experimental/Cleanup/NanoLogger/Configuration/NanoLoggerConfiguration.cs
+++ Common/Abbotware.Experimental/Cleanup/NanoLogger/Configuration/NanoLoggerConfiguration.cs
@@ -1,7 +1,7 @@
-//--------------------------------------------------------------------
+﻿//--------------------------------------------------------------------
 // -----------------------------------------------------------------------
 // <copyright file="NanoLoggerConfiguration.cs" company="Abbotware, LLC">
-// Copyright � Abbotware, LLC 2012-2020. All rights reserved
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
 // <author>Anthony Abate</author>
