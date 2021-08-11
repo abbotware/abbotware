5c367a54ee072bb4b01ced00b88d73166c7f345f
Anthony Abate <anthony.abate@gmail.com>
2021-08-10 21:51:32 -0400

    add overloads for deferred evaluation

--- Common/Abbotware.Core/Logging/LoggerExtensions.cs
+++ Common/Abbotware.Core/Logging/LoggerExtensions.cs
@@ -29,6 +29,19 @@ namespace Abbotware.Core.Logging
         /// <param name="file">compiler injected file</param>
         public static void Critical([NotNull] this ILogger2 logger, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
+            logger.Critical(null, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Critical level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Critical([NotNull] this ILogger2 logger, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
             logger.Critical(null, message, line, member, file);
         }
 
@@ -43,6 +56,20 @@ namespace Abbotware.Core.Logging
         /// <param name="file">compiler injected file</param>
         public static void Critical([NotNull] this ILogger2 logger, Exception? exception, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
+            Critical(logger!, exception, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Critical level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="exception">exception</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Critical([NotNull] this ILogger2 logger, Exception? exception, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
             Log(logger!, LogLevel.Critical, exception, message, line, member, file);
         }
 
@@ -55,6 +82,19 @@ namespace Abbotware.Core.Logging
         /// <param name="member">compiler injected class member</param>
         /// <param name="file">compiler injected file</param>
         public static void Error([NotNull] this ILogger2 logger, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
+            logger.Error(null, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Error level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Error([NotNull] this ILogger2 logger, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
             logger.Error(null, message, line, member, file);
         }
@@ -70,6 +110,20 @@ namespace Abbotware.Core.Logging
         /// <param name="file">compiler injected file</param>
         public static void Error([NotNull] this ILogger2 logger, Exception? exception, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
+            logger.Error(exception, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Error level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="exception">exception</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Error([NotNull] this ILogger2 logger, Exception? exception, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
             Log(logger, LogLevel.Error, exception, message, line, member, file);
         }
 
@@ -82,6 +136,19 @@ namespace Abbotware.Core.Logging
         /// <param name="member">compiler injected class member</param>
         /// <param name="file">compiler injected file</param>
         public static void Warn([NotNull] this ILogger2 logger, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
+            logger.Warn(null, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Warning level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Warn([NotNull] this ILogger2 logger, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
             logger.Warn(null, message, line, member, file);
         }
@@ -97,6 +164,20 @@ namespace Abbotware.Core.Logging
         /// <param name="file">compiler injected file</param>
         public static void Warn([NotNull] this ILogger2 logger, Exception? exception, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
+            logger.Warn(exception, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Warning level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="exception">exception</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Warn([NotNull] this ILogger2 logger, Exception? exception, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
             Log(logger, LogLevel.Warning, exception, message, line, member, file);
         }
 
@@ -109,6 +190,19 @@ namespace Abbotware.Core.Logging
         /// <param name="member">compiler injected class member</param>
         /// <param name="file">compiler injected file</param>
         public static void Info([NotNull] this ILogger2 logger, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
+            logger.Error(null, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Information level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Info([NotNull] this ILogger2 logger, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
             logger.Error(null, message, line, member, file);
         }
@@ -124,7 +218,21 @@ namespace Abbotware.Core.Logging
         /// <param name="file">compiler injected file</param>
         public static void Info([NotNull] this ILogger2 logger, Exception? exception, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
-            Log(logger, LogLevel.Information, exception, message, line, member, file);
+            Log(logger, LogLevel.Information, exception, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Information level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="exception">exception</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Info([NotNull] this ILogger2 logger, Exception? exception, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
+            logger.Info(exception, message, line, member, file);
         }
 
         /// <summary>
@@ -136,6 +244,19 @@ namespace Abbotware.Core.Logging
         /// <param name="member">compiler injected class member</param>
         /// <param name="file">compiler injected file</param>
         public static void Debug([NotNull] this ILogger2 logger, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
+            logger.Debug(null, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Debug level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Debug([NotNull] this ILogger2 logger, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
             logger.Debug(null, message, line, member, file);
         }
@@ -151,7 +272,21 @@ namespace Abbotware.Core.Logging
         /// <param name="file">compiler injected file</param>
         public static void Debug([NotNull] this ILogger2 logger, Exception? exception, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
-            Log(logger, LogLevel.Debug, exception, message, line, member, file);
+            Log(logger, LogLevel.Debug, exception, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Debug level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="exception">exception</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        public static void Debug([NotNull] this ILogger2 logger, Exception? exception, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
+            logger.Debug(exception, message, line, member, file);
         }
 
         /// <summary>
@@ -164,6 +299,20 @@ namespace Abbotware.Core.Logging
         /// <param name="file">compiler injected file</param>
         [Conditional("TRACE")]
         public static void Trace([NotNull] this ILogger2 logger, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
+            logger.Trace(null, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Trace level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        [Conditional("TRACE")]
+        public static void Trace([NotNull] this ILogger2 logger, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
             logger.Trace(null, message, line, member, file);
         }
@@ -180,6 +329,21 @@ namespace Abbotware.Core.Logging
         [Conditional("TRACE")]
         public static void Trace([NotNull] this ILogger2 logger, Exception? exception, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
+            logger.Trace(exception, () => message, line, member, file);
+        }
+
+        /// <summary>
+        /// Log a message at Debug level
+        /// </summary>
+        /// <param name="logger">extended logger</param>
+        /// <param name="exception">exception</param>
+        /// <param name="message">message function for deferred evaluation</param>
+        /// <param name="line">compiler injected line number</param>
+        /// <param name="member">compiler injected class member</param>
+        /// <param name="file">compiler injected file</param>
+        [Conditional("TRACE")]
+        public static void Trace([NotNull] this ILogger2 logger, Exception? exception, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        {
             Log(logger, LogLevel.Trace, exception, message, line, member, file);
         }
 
@@ -193,11 +357,16 @@ namespace Abbotware.Core.Logging
         /// <param name="line">compiler injected line number</param>
         /// <param name="member">compiler injected class member</param>
         /// <param name="file">compiler injected file</param>
-        private static void Log([NotNull] ILogger2 logger, LogLevel level, Exception? exception, string message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
+        private static void Log([NotNull] ILogger2 logger, LogLevel level, Exception? exception, Func<string> message, [CallerLineNumber] int? line = null, [CallerMemberName] string? member = null, [CallerFilePath] string? file = null)
         {
+            if (!logger.IsEnabled(level))
+            {
+                return;
+            }
+
             var lld = new LogLocationDetail(line, member, file);
 
-            logger.Log(level, default, lld, exception, (s, e) => message);
+            logger.Log(level, default, lld, exception, (s, e) => message());
         }
     }
 }
