2c3546d4523f4e3ecec49deb96464185a12f5cb8
Anthony Abate <anthony.abate@gmail.com>
2021-09-05 10:24:20 -0400

    Add collation constant

---
+++ Interop/Abbotware.Interop.EntityFramework/Collation.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Collation.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.EntityFramework
+{
+    /// <summary>
+    /// Collation Constants
+    /// </summary>
+    public static class Collation
+    {
+        /// <summary>
+        /// latin1 - Code Page 1252 - case sensitive - accent sensitive
+        /// </summary>
+        public const string SQL_Latin1_General_CP1_CS_AS = "SQL_Latin1_General_CP1_CS_AS";
+    }
+}
---
+++ Interop/Abbotware.Interop.EntityFramework/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GlobalSuppressions.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2020. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Reviewed AA", Scope = "member", Target = "~F:Abbotware.Interop.EntityFramework.Collation.SQL_Latin1_General_CP1_CS_AS")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "Reviewed AA", Scope = "member", Target = "~F:Abbotware.Interop.EntityFramework.Collation.SQL_Latin1_General_CP1_CS_AS")]
