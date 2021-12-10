0b74923b122940fa6a7c53b7da68d1ffb0dfa083
Anthony Abate <anthony.abate@gmail.com>
2021-12-10 13:23:41 -0500

    fix unit tests

--- Interop/Abbotware.Interop.Iso/IsoHelpers.cs
+++ Interop/Abbotware.Interop.Iso/IsoHelpers.cs
@@ -14,11 +14,11 @@ namespace Abbotware.Interop.Iso
         /// <summary>
         /// Gets a singleton instance of the CurrencyMetadataLookup
         /// </summary>
-        public static CurrencyMetadataLookup Currency => new();
+        public static CurrencyMetadataLookup Currency { get; } = new();
 
         /// <summary>
         /// Gets a singleton instance of the CountryMetadataLookup
         /// </summary>
-        public static CountryMetadataLookup Country => new();
+        public static CountryMetadataLookup Country { get; } = new();
     }
 }
