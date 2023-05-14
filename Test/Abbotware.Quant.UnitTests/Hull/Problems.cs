1270aac45713e90a44654c686ed8a84539b5dfe1
Anthony Abate <anthony.abate@gmail.com>
2023-05-14 13:11:48 -0400

    some nuget updates

---
+++ Test/Abbotware.Quant.UnitTests/Hull/Problems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbotware.Quant.UnitTests.Hull
+{
+    internal class Problems
+    {
+        public Problems() { }
+    }
+}
---
+++ Test/Abbotware.Quant.UnitTests/Quant/PayoffTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Abbotware.Quant.UnitTests.Quant
+{
+    using Abbotware.Quant.Payoffs.Plugins;
+    using NUnit.Framework;
+
+    public class PayoffTests
+    {
+        [Test]
+        [TestCase(56.2, 100.3, ExpectedResult = 44.1)]
+        [TestCase(100.3, 56.2, ExpectedResult = -44.1)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(10, 5, ExpectedResult = -5)]
+        [TestCase(1, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = -1)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = 1)]
+        public decimal LongTests(decimal strike, decimal spot)
+        {
+            var p = new LongPayoff(strike);
+
+            return p.Compute(spot);
+        }
+
+        [Test]
+        [TestCase(56.2, 100.3, ExpectedResult = -44.1)]
+        [TestCase(100.3, 56.2, ExpectedResult = 44.1)]
+        [TestCase(5, 10, ExpectedResult = -5)]
+        [TestCase(10, 5, ExpectedResult = 5)]
+        [TestCase(1, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 1)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = -1)]
+        public decimal ShortTests(decimal strike, decimal spot)
+        {
+            var p = new ShortPayoff(strike);
+
+            return p.Compute(spot);
+        }
+
+        [Test]
+        [TestCase(56.2, 100.3, ExpectedResult = 44.1)]
+        [TestCase(100.3, 56.2, ExpectedResult = 0)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(10, 5, ExpectedResult = 0)]
+        [TestCase(1, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 0)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = 1)]
+        public decimal CallTests(decimal strike, decimal spot)
+        {
+            var p = new CallPayoff(strike);
+
+            return p.Compute(spot);
+        }
+
+        [Test]
+        [TestCase(56.2, 100.3, ExpectedResult = 0)]
+        [TestCase(100.3, 56.2, ExpectedResult = 44.1)]
+        [TestCase(5, 10, ExpectedResult = 0)]
+        [TestCase(10, 5, ExpectedResult = 5)]
+        [TestCase(1, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 1)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = 0)]
+        public decimal PutTests(decimal strike, decimal spot)
+        {
+            var p = new PutPayoff(strike);
+
+            return p.Compute(spot);
+        }
+    }
+}
