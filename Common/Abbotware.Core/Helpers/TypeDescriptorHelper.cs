5113320e2e9104b044ffa571a8688a581386fcff
Anthony Abate <anthony.abate@gmail.com>
2023-06-04 18:54:14 -0400

    cache of converters

---
+++ Common/Abbotware.Core/Helpers/TypeDescriptorHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Abbotware.Core.Helpers
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// TypeDescripter helper methods
+    /// </summary>
+    public static class TypeDescriptorHelper
+    {
+        private static readonly ConcurrentDictionary<Type, TypeConverter> Cache = new();
+
+        /// <summary>
+        /// Gets the TypeConverter from the cache
+        /// </summary>
+        /// <param name="source">source type</param>
+        /// <returns>converter</returns>
+        public static TypeConverter GetConverter(Type source) => Cache.GetOrAdd(source, TypeDescriptor.GetConverter(source));
+    }
+}
