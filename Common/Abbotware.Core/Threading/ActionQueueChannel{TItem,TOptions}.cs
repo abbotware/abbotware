306212aa83a52b78582f06e40c407c98841e6dfc
Anthony Abate <anthony.abate@gmail.com>
2023-06-10 15:47:08 -0400

    new System.Threading.Channel queue 'wrappers'

---
+++ Common/Abbotware.Core/Threading/ActionQueueChannel{TItem,TOptions}.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ActionQueueChannel{TItem,TOptions}.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Core.Threading
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Channels;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    ///  Channel based Queue that takes an action
+    /// </summary>
+    /// <typeparam name="TItem">item type</typeparam>
+    /// <typeparam name="TOptions">channel options</typeparam>
+    public class ActionQueueChannel<TItem, TOptions> : QueueChannel<TItem, TOptions>
+    where TOptions : ChannelOptions
+    {
+        private readonly Func<TItem, CancellationToken, ValueTask> action;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActionQueueChannel{TItem, TOptions}"/> class.
+        /// </summary>
+        /// <param name="action">item processing action</param>
+        /// <param name="options">channel options</param>
+        /// <param name="logger">injected logger</param>
+        public ActionQueueChannel(Func<TItem, CancellationToken, ValueTask> action, TOptions options, ILogger<ActionQueueChannel<TItem, TOptions>> logger)
+            : base(options, logger)
+        {
+            this.action = action;
+        }
+
+        /// <inheritdoc/>
+        protected override ValueTask OnItemAsync(TItem data, CancellationToken ct)
+        {
+            return this.action(data, ct);
+        }
+    }
+}
---
+++ Common/Abbotware.Core/Threading/QueueChannel{TItem,TOptions}.cs
@@ -0,0 +1,103 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="QueueChannel{TItem,TOptions}.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Core.Threading
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Channels;
+    using System.Threading.Tasks;
+    using Abbotware.Core.Objects;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// base class for a Channel based Queue
+    /// </summary>
+    /// <typeparam name="TItem">item type</typeparam>
+    /// <typeparam name="TOptions">channel options</typeparam>
+    public abstract class QueueChannel<TItem, TOptions> : BaseAsyncComponent
+        where TOptions : ChannelOptions
+    {
+        private readonly Channel<TItem> channel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueueChannel{TData, TOptions}"/> class.
+        /// </summary>
+        /// <param name="options">channel options</param>
+        /// <param name="logger">injected logger</param>
+        protected QueueChannel(TOptions options, ILogger<QueueChannel<TItem, TOptions>> logger)
+            : base(logger)
+        {
+            switch (options)
+            {
+                case BoundedChannelOptions bco:
+                    this.channel = Channel.CreateBounded<TItem>(bco);
+                    break;
+                case UnboundedChannelOptions uco:
+                    this.channel = Channel.CreateUnbounded<TItem>(uco);
+                    break;
+                default:
+                    throw new NotSupportedException($"{options} is not supported");
+            }
+
+            _ = this.Start();
+        }
+
+        /// <summary>
+        /// Enqueues data
+        /// </summary>
+        /// <param name="data">data item</param>
+        /// <param name="ct">cancellation token</param>
+        /// <returns>async task</returns>
+        public ValueTask EnqueueAsync(TItem data, CancellationToken ct)
+        {
+            return this.EnqueueAsync(new[] { data }, ct);
+        }
+
+        /// <summary>
+        /// Enqueues data
+        /// </summary>
+        /// <param name="data">data items</param>
+        /// <param name="ct">cancellation token</param>
+        /// <returns>async task</returns>
+        public async ValueTask EnqueueAsync(TItem[] data, CancellationToken ct)
+        {
+            foreach (var d in data)
+            {
+                await this.channel.Writer.WriteAsync(d, ct)
+                    .ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// hook to implement item processing logic
+        /// </summary>
+        /// <param name="data">data item to process</param>
+        /// <param name="ct">cancellation token</param>
+        /// <returns>async task</returns>
+        protected abstract ValueTask OnItemAsync(TItem data, CancellationToken ct);
+
+        private async Task Start()
+        {
+            while (await this.channel.Reader.WaitToReadAsync(this.DisposeRequested.Token)
+                .ConfigureAwait(false))
+            {
+                ////await foreach (var data in this.channel.Reader.ReadAllAsync(this.DisposeRequested.Token))
+                ////{
+                ////    try
+                ////    {
+                ////        await this.OnItemAsync(data, this.DisposeRequested.Token)
+                ////            .ConfigureAwait(false);
+                ////    }
+                ////    catch (Exception ex)
+                ////    {
+                ////        this.Logger.Error(ex, "OnItemAsync");
+                ////    }
+                ////}
+            }
+        }
+    }
+}
