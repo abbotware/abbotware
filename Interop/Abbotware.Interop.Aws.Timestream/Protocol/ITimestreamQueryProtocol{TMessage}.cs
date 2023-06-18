c5ce76ae22adb4a376045638ada1df0edfd42330
Anthony Abate <anthony.abate@gmail.com>
2023-06-18 16:39:25 -0400

    split into encode / decode interface

--- Interop/Abbotware.Interop.Aws.Timestream/Protocol/ITimestreamQueryProtocol{TMessage}.cs
+++ Interop/Abbotware.Interop.Aws.Timestream/Protocol/ITimestreamQueryProtocol{TMessage}.cs
@@ -1,16 +1,19 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="ITimestreamProtocol{TMessage}.cs" company="Abbotware, LLC">
+// <copyright file="ITimestreamQueryProtocol{TMessage}.cs" company="Abbotware, LLC">
 // Copyright © Abbotware, LLC 2012-2023. All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
 
 namespace Abbotware.Interop.Aws.Timestream.Protocol
 {
+    using Abbotware.Core.Serialization;
+    using Amazon.TimestreamQuery.Model;
+
     /// <summary>
-    /// interface for decoding / encoding Timestream messages
+    /// interface for a protocol that decodes Timestream Query response messages
     /// </summary>
     /// <typeparam name="TMessage">message type</typeparam>
-    public interface ITimestreamProtocol<TMessage> : ITimestreamWriteProtocol<TMessage>, ITimestreamQueryProtocol<TMessage>
+    public interface ITimestreamQueryProtocol<TMessage> : IDecode<TMessage, Row>, IBulkDecode<TMessage, QueryResponse>
         where TMessage : notnull
     {
     }
---
+++ Interop/Abbotware.Interop.Aws.Timestream/Protocol/ITimestreamWriteProtocol{TMessage}.cs
@@ -0,0 +1,72 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ITimestreamWriteProtocol{TMessage}.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.Aws.Timestream.Protocol
+{
+    using System.Collections.Generic;
+    using Abbotware.Core.Serialization;
+    using Abbotware.Interop.Aws.Timestream.Configuration;
+    using Abbotware.Interop.Aws.Timestream.Protocol.Internal;
+    using Amazon.TimestreamWrite.Model;
+
+    /// <summary>
+    /// interface for a protocol that encodes Timestream Write request messages
+    /// </summary>
+    /// <typeparam name="TMessage">message type</typeparam>
+    public interface ITimestreamWriteProtocol<TMessage> : IEncode<TMessage, WriteRecordsRequest>, IBulkEncode<TMessage, WriteRecordsRequest>
+        where TMessage : notnull
+    {
+        /// <summary>
+        /// Encodes a message
+        /// </summary>
+        /// <param name="message">message to encode</param>
+        /// <param name="options">timestream options</param>
+        /// <returns>encoded write request</returns>
+        WriteRecordsRequest Encode(TMessage message, TimestreamOptions options);
+
+        /// <summary>
+        /// Encodes a message
+        /// </summary>
+        /// <param name="message">message to encode</param>
+        /// <param name="timestamp">timestamp lookup function</param>
+        /// <returns>encoded write request</returns>
+        WriteRecordsRequest Encode(TMessage message, IRecordUpdater<TMessage> timestamp);
+
+        /// <summary>
+        /// Encodes a message
+        /// </summary>
+        /// <param name="message">message to encode</param>
+        /// <param name="options">timestream options</param>
+        /// <param name="timestamp">timestamp lookup function</param>
+        /// <returns>encoded write request</returns>
+        WriteRecordsRequest Encode(TMessage message, TimestreamOptions options, IRecordUpdater<TMessage> timestamp);
+
+        /// <summary>
+        ///  Encodes a batch of messages
+        /// </summary>
+        /// <param name="messages">messages to encode</param>
+        /// <param name="options">timestream options</param>
+        /// <returns>encoded write request</returns>
+        WriteRecordsRequest Encode(IEnumerable<TMessage> messages, TimestreamOptions options);
+
+        /// <summary>
+        ///  Encodes a batch of messages
+        /// </summary>
+        /// <param name="messages">messages to encode</param>
+        /// <param name="timestamp">timestamp lookup function</param>
+        /// <returns>encoded write request</returns>
+        WriteRecordsRequest Encode(IEnumerable<TMessage> messages, IRecordUpdater<TMessage> timestamp);
+
+        /// <summary>
+        /// Encodes a batch of messages
+        /// </summary>
+        /// <param name="messages">messages to encode</param>
+        /// <param name="options">timestream options</param>
+        /// <param name="timestamp">timestamp lookup function</param>
+        /// <returns>encoded write request</returns>
+        WriteRecordsRequest Encode(IEnumerable<TMessage> messages, TimestreamOptions options, IRecordUpdater<TMessage> timestamp);
+    }
+}
