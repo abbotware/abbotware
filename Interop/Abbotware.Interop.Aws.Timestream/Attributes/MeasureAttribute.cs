e72fdd55b44f6f6c6d66696bce03311b35d87528
Anthony Abate <anthony.abate@gmail.com>
2023-11-15 21:27:06 -0500

    warning fix

--- Interop/Abbotware.Interop.Aws.Timestream/Attributes/MeasureAttribute.cs
+++ Interop/Abbotware.Interop.Aws.Timestream/Attributes/MeasureAttribute.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="MeasureValueAttribute.cs" company="Abbotware, LLC">
+// <copyright file="MeasureAttribute.cs" company="Abbotware, LLC">
 // Copyright © Abbotware, LLC 2012-2023. All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
