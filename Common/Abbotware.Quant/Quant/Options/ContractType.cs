65fa8c43db82c04706e7fbe415a46c5b328354ad
Anthony Abate <anthony.abate@gmail.com>
2024-02-12 01:24:29 -0500

    Black-Scholes formulas

---
+++ Common/Abbotware.Quant/Quant/Options/ContractType.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ContractType.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Quant.Options
+{
+    /// <summary>
+    /// Option Contract Type
+    /// </summary>
+    public enum ContractType
+    {
+        /// <summary>
+        /// Unknown contract type
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Call Contract
+        /// </summary>
+        Call = 1,
+
+        /// <summary>
+        /// Put Contract
+        /// </summary>
+        Put = 2,
+    }
+}
