3837248dee442e063a806a82f03c02b59bd3e55c
Abbotware <abbotware@users.noreply.github.com>
2024-03-17 15:34:26 -0400

    some analysis functions (#13)

---
+++ Common/Abbotware.Quant/Statistics/ErrorAnalysis.cs
@@ -0,0 +1,53 @@
+﻿namespace Abbotware.Quant.Statistics
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Statistics Error Analysis
+    /// </summary>
+    public static class ErrorAnalysis
+    {
+        /// <summary>
+        /// Compute the Absolute Percentage Error
+        /// </summary>
+        /// <typeparam name="T">numeric type</typeparam>
+        /// <param name="actual">actual value</param>
+        /// <param name="forecast">forcated value</param>
+        /// <returns>Absolute Percentage Error</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T AbsolutePercentageError<T>(T actual, T forecast)
+             where T : INumber<T>
+        {
+            var delta = actual - forecast;
+            var abs = T.Abs(delta);
+            var e = abs / actual;
+            return e;
+        }
+
+        /// <summary>
+        /// Compute the Mean Absolute Percentage Error (MAPE) / Mean Absolute Percentage Deviation (MAPD)
+        /// </summary>
+        /// <remarks>https://en.wikipedia.org/wiki/Mean_absolute_percentage_error</remarks>
+        /// <typeparam name="T">numeric type</typeparam>
+        /// <param name="actuals">actual values</param>
+        /// <param name="forecasts">forecasted values</param>
+        /// <returns>Mean Absolute Percentage Error</returns>
+        public static T MeanAbsolutePercentageError<T>(IEnumerable<T> actuals, IEnumerable<T> forecasts)
+             where T : INumber<T>
+        {
+            var sum = T.Zero;
+            var n = T.Zero;
+
+            foreach (var (a, f) in actuals.Zip(forecasts))
+            {
+                sum += AbsolutePercentageError(a, f);
+                n += T.One;
+            }
+
+            return sum / n;
+        }
+    }
+}
