d1b93a325dd6dbfdb557eff6f58a65435da815af
Anthony Abate <anthony.abate@gmail.com>
2024-12-08 02:17:54 -0500

    sec api

---
+++ Interop/Abbotware.Interop.SecApi/ISecApiMappingClient.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISecApiMappingClient.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.SecApi
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Abbotware.Core.Net.Http;
+    using Abbotware.Interop.SecApi.Model;
+
+    /// <summary>
+    /// Fundamentals subset api
+    /// </summary>
+    public interface ISecApiMappingClient
+    {
+        /// <summary>
+        /// retrieve Comapny Details via CUSIP
+        /// </summary>
+        /// <param name="cusip">CUSIP code</param>
+        /// <param name="ct">cancellation token</param>
+        /// <returns>search result</returns>
+        Task<RestResponse<CompanyDetails[], ErrorMessage>> CusipAsync(string cusip, CancellationToken ct);
+    }
+}
---
+++ Interop/Abbotware.Interop.SecApi/Model/ErrorMessage.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ErrorMessage.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.SecApi.Model
+{
+    /// <summary>
+    /// Error Message
+    /// </summary>
+    /// <param name="Status">status code</param>
+    /// <param name="Error">error text</param>
+    public record ErrorMessage(
+        long Status,
+        string Error);
+}
