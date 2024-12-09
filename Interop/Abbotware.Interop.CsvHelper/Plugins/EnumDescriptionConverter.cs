e4b4cb7e4465486fb0ad6f42adcf6aacebfff9bf
Abbotware <abbotware@users.noreply.github.com>
2024-12-08 21:15:57 -0500

    helpers (#24)

---
+++ Interop/Abbotware.Interop.CsvHelper/Plugins/EnumDescriptionConverter.cs
@@ -0,0 +1,69 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EnumDescriptionConverter.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.CsvHelper.Plugins;
+
+using System;
+using System.ComponentModel;
+using System.Reflection;
+using global::CsvHelper;
+using global::CsvHelper.Configuration;
+using global::CsvHelper.TypeConversion;
+
+/// <summary>
+/// Converts an enum based on the [Description] attribute
+/// </summary>
+/// <typeparam name="TEnum">enum type</typeparam>
+public class EnumDescriptionConverter<TEnum> : DefaultTypeConverter
+    where TEnum : Enum
+{
+    /// <summary>
+    /// Gets a value indicating whether or not support null
+    /// </summary>
+    protected bool Nullable { get; init; }
+
+    /// <inheritdoc/>
+    public override object? ConvertFromString(string? text, IReaderRow row, MemberMapData memberMapData)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            if (this.Nullable)
+            {
+                return null;
+            }
+            else
+            {
+                throw new ArgumentException($"Cannot convert '{text}' to {typeof(TEnum).Name}");
+            }
+        }
+
+        var fields = typeof(TEnum).GetFields();
+
+        foreach (var field in fields)
+        {
+            var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+            if (attribute != null && attribute.Description.Equals(text, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return Enum.Parse(typeof(TEnum), field.Name);
+            }
+        }
+
+        throw new ArgumentException($"Cannot convert '{text}' to {typeof(TEnum).Name}");
+    }
+
+    /// <inheritdoc/>
+    public override string? ConvertToString(object? value, IWriterRow row, MemberMapData memberMapData)
+    {
+        if (value is TEnum enumValue)
+        {
+            var field = typeof(TEnum).GetField(enumValue.ToString());
+            var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+            return attribute?.Description ?? enumValue.ToString();
+        }
+
+        return base.ConvertToString(value, row, memberMapData);
+    }
+}
---
+++ Interop/Abbotware.Interop.CsvHelper/Plugins/NullableEnumDescriptionConverter.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NullableEnumDescriptionConverter.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.CsvHelper.Plugins;
+
+using System;
+
+/// <summary>
+/// Converts an enum based on the [Description] attribute - supports null
+/// </summary>
+/// <typeparam name="TEnum">enum type</typeparam>
+public class NullableEnumDescriptionConverter<TEnum> : EnumDescriptionConverter<TEnum>
+    where TEnum : Enum
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NullableEnumDescriptionConverter{T}"/> class.
+    /// </summary>
+    public NullableEnumDescriptionConverter()
+    {
+        this.Nullable = true;
+    }
+}
