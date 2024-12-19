72a9acae046ecb34fbe670a50a8484f7e10311ab
Abbotware <abbotware@users.noreply.github.com>
2024-12-18 22:40:55 -0500

    parsers (#27)

---
+++ Common/Abbotware.Quant/SecurityIdentifiers/CheckDigit.cs
@@ -0,0 +1,62 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CheckDigit.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Quant.SecurityIdentifiers;
+
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Helper methods for Check Digit computation
+/// </summary>
+public static class CheckDigit
+{
+    /// <summary>
+    /// Gets the Modulo 10 Complement
+    /// </summary>
+    /// <param name="value">value to complement</param>
+    /// <returns>modulo 10 Complement</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static int Modulo10Complement(sbyte value)
+        => ModuloComplement(value, 10);
+
+    /// <summary>
+    /// Gets the Modulo 10 Complement
+    /// </summary>
+    /// <param name="value">value to complement</param>
+    /// <param name="mod">modulus</param>
+    /// <returns>modulo 10 Complement</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static int ModuloComplement(sbyte value, int mod)
+        => (mod - (value % mod)) % mod;
+
+    /// <summary>
+    /// Adds the Digits of a number
+    /// </summary>
+    /// <remarks>12 => 1 + 2 = 3</remarks>
+    /// <param name="value">value to sum</param>
+    /// <returns>sum of digits</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static sbyte DigitSum(int value)
+        => value switch
+        {
+            < 10 => (sbyte)value,
+            < 100 => (sbyte)((value / 10) + (value % 10)),
+            _ => GeneralDigitSum(value),
+        };
+
+    private static sbyte GeneralDigitSum(int value)
+    {
+        var sum = 0;
+
+        while (value > 0)
+        {
+            sum += value % 10;
+            value /= 10;
+        }
+
+        return (sbyte)sum;
+    }
+}
---
+++ Test/Abbotware.Quant.UnitTests/SecuirtyIdentifiers/FigiTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Abbotware.UnitTests.Quant.SecuirtyIdentifiers;
+
+using System;
+using Abbotware.Quant.SecurityIdentifiers;
+using NUnit.Framework;
+
+[TestFixture]
+public class FigiTests
+{
+    [TestCase("BBG000BLNNH6", ExpectedResult = true)] // Apple Inc.
+    [TestCase("BBG000B9XRY4", ExpectedResult = true)] // Microsoft Corporation
+    [TestCase("BBG001SCTQY4", ExpectedResult = true)] // Meta Platforms Inc. (Facebook)
+    [TestCase("BBG00178PGX3", ExpectedResult = true)] // Berkshire Hathaway Class B
+    [TestCase("BBG000BLNNH6", ExpectedResult = true)] // Valid FIGI
+
+    // Invalid FIGIs
+    [TestCase("BBG000BLNNHX", ExpectedResult = false)] // Invalid check digit (not numeric)
+    [TestCase("BBG000BLNNH5", ExpectedResult = false)] // Invalid checksum
+    [TestCase("BBG000BLNNH7", ExpectedResult = false)] // Invalid checksum
+    [TestCase("BBG000B9XRY5", ExpectedResult = false)] // Invalid checksum
+    [TestCase("BBG000F2CWY4", ExpectedResult = false)] // Invalid checksum
+    [TestCase("BBG000BLNNH", ExpectedResult = false)] // Too short
+    [TestCase("BBG000BLNNH67", ExpectedResult = false)] // Too long
+    [TestCase("BBG000B9XRA4", ExpectedResult = false)] // Contains vowel 'A'
+    [TestCase("BBO000BLNNH6", ExpectedResult = false)] // Contains vowel 'O'
+
+    // Structural rule violations
+    [TestCase("BSG000BLNNH6", ExpectedResult = false)] // Invalid prefix 'BS'
+    [TestCase("BMG000BLNNH6", ExpectedResult = false)] // Invalid prefix 'BM'
+    [TestCase("GGG000BLNNH6", ExpectedResult = false)] // Invalid prefix 'GG'
+    [TestCase("VGG000BLNNH6", ExpectedResult = false)] // Invalid prefix 'VG'
+    [TestCase("BBT100BLNNH6", ExpectedResult = false)] // Invalid third character
+    public bool Figi_IsValid_Tests(string figi)
+        => Figi.IsValid(figi.AsSpan());
+
+    [Test]
+    public void Figi_IsValid_HandlesNullOrEmpty()
+    {
+        Assert.That(() => Figi.IsValid([]), Is.False);
+        Assert.That(() => Figi.IsValid(string.Empty.AsSpan()), Is.False);
+    }
+}
