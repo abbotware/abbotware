dd4a5c743866e0394f88b4f6fcdab11dfff866d5
Anthony Abate <anthony.abate@gmail.com>
2025-04-14 16:03:24 -0400

    new files

---
+++ Interop/Abbotware.Interop.Microsoft/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensions.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Interop.Microsoft.Extensions;
+
+using System;
+using Abbotware.Interop.Microsoft.Internal;
+using global::Microsoft.Extensions.DependencyInjection;
+using global::Microsoft.Extensions.Options;
+
+/// <summary>
+/// Extension methods for IServiceCollection
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds an immutable option class via a callback factory
+    /// </summary>
+    /// <typeparam name="TOptions">options type</typeparam>
+    /// <param name="extended">target for extension method</param>
+    /// <param name="factory">options factory</param>
+    /// <returns>service collection</returns>
+    public static IServiceCollection AddRecordAsOptions<TOptions>(this IServiceCollection extended, Func<string, TOptions> factory)
+      where TOptions : class =>
+        extended
+            .Configure<TOptions>(a => { })
+            .AddSingleton<IOptionsFactory<TOptions>>(new CallbackOptionsFactory<TOptions>(factory));
+}
