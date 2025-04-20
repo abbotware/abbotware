521b51c4f8f1a3914a4769d4cf335fe12ca5cd16
Anthony Abate <anthony.abate@gmail.com>
2025-04-20 19:51:47 -0400

    ext method for options

--- Interop/Abbotware.Interop.Microsoft/Extensions/ServiceCollectionExtensions.cs
+++ Interop/Abbotware.Interop.Microsoft/Extensions/ServiceCollectionExtensions.cs
@@ -17,15 +17,27 @@ using global::Microsoft.Extensions.Options;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
+    /// Adds an immutable option class via a callback factory - allows for named instances
+    /// </summary>
+    /// <typeparam name="TOptions">options type</typeparam>
+    /// <param name="extended">target for extension method</param>
+    /// <param name="name">The name of the <typeparamref name="TOptions"/> instance to create.</param>
+    /// <param name="factory">options factory</param>
+    /// <returns>service collection</returns>
+    public static IServiceCollection AddRecordAsOptions<TOptions>(this IServiceCollection extended, string name, Func<string, TOptions> factory)
+      where TOptions : class =>
+        extended
+            .Configure<TOptions>(name, a => { })
+            .AddSingleton<IOptionsFactory<TOptions>>(new CallbackOptionsFactory<TOptions>(factory));
+
+    /// <summary>
     /// Adds an immutable option class via a callback factory
     /// </summary>
     /// <typeparam name="TOptions">options type</typeparam>
     /// <param name="extended">target for extension method</param>
     /// <param name="factory">options factory</param>
     /// <returns>service collection</returns>
-    public static IServiceCollection AddRecordAsOptions<TOptions>(this IServiceCollection extended, Func<string, TOptions> factory)
+    public static IServiceCollection AddRecordAsOptions<TOptions>(this IServiceCollection extended, Func<TOptions> factory)
       where TOptions : class =>
-        extended
-            .Configure<TOptions>(a => { })
-            .AddSingleton<IOptionsFactory<TOptions>>(new CallbackOptionsFactory<TOptions>(factory));
+        extended.AddRecordAsOptions(string.Empty, _ => factory());
 }
