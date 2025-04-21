c35038989a60b501f2dd02f7b631def840abb45d
Anthony Abate <anthony.abate@gmail.com>
2025-04-20 20:29:32 -0400

    exclude from code coverage

--- Common/Abbotware.Experimental/GlobalSuppressions.cs
+++ Common/Abbotware.Experimental/GlobalSuppressions.cs
@@ -6,6 +6,8 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: ExcludeFromCodeCoverage]
+
 [assembly: SuppressMessage("Security", "CA2109:Review visible event handlers", Justification = "Reviewed AA", Scope = "member", Target = "~M:Abbotware.Interop.RabbitMQ.ExtensionPoints.BaseChannelManager.OnBasicAck(System.Object,RabbitMQ.Client.Events.BasicAckEventArgs)")]
 [assembly: SuppressMessage("Security", "CA2109:Review visible event handlers", Justification = "Reviewed AA", Scope = "member", Target = "~M:Abbotware.Interop.RabbitMQ.ExtensionPoints.BaseChannelManager.OnBasicReturn(System.Object,RabbitMQ.Client.Events.BasicReturnEventArgs)")]
 [assembly: SuppressMessage("Security", "CA2109:Review visible event handlers", Justification = "Reviewed AA", Scope = "member", Target = "~M:Abbotware.Interop.RabbitMQ.ExtensionPoints.BaseChannelManager.OnModelShutdown(System.Object,RabbitMQ.Client.ShutdownEventArgs)")]
