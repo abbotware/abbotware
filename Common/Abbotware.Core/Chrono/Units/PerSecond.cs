e2d6b9f55dfd2c3ff9e551cf7a62d4c6ffcac729
Anthony Abate <anthony.abate@gmail.com>
2025-05-11 11:52:56 -0400

    constants for conversion calcs

---
+++ Common/Abbotware.Core/Chrono/Units/PerSecond.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PerSecond.cs" company="Abbotware, LLC">
+// Copyright © Abbotware, LLC 2012-2023. All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Abbotware.Core.Chrono.Units;
+
+/// <summary>
+/// Per/Second Time Unit Constants
+/// </summary>
+public static class PerSecond
+{
+    /// <summary>
+    /// Units Constant for Number of Milliseconds in a Second
+    /// </summary>
+    public const double Milliseconds = 1_000d;
+
+    /// <summary>
+    /// Units Constant for Number of Microseconds in a Second
+    /// </summary>
+    public const double Microseconds = 1_000_000d;
+
+    /// <summary>
+    /// Units Constant for Number of Nanoseconds in a Second
+    /// </summary>
+    public const double Nanoseconds = 1_000_000_000d;
+}
