ea8e6fbdc1509dece043316110968d87d9d1a2a4
Anthony Abate <anthony.abate@gmail.com>
2025-05-11 11:52:34 -0400

    cleaner hi-res timing helpers

--- Common/Abbotware.Core/Chrono/HighResolutionStopWatch.cs
+++ Common/Abbotware.Core/Chrono/HighResolutionStopWatch.cs
@@ -4,46 +4,279 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Abbotware.Core.Chrono
+namespace Abbotware.Core.Chrono;
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Abbotware.Core.Chrono.Units;
+
+/// <summary>
+/// Stopwatch that returns HighResolutionTimeSpans
+/// </summary>
+public class HighResolutionStopWatch
 {
-    using System.Runtime.CompilerServices;
+    private HighResolutionStopWatch(long start)
+    {
+        this.Start = start;
+    }
 
     /// <summary>
-    /// Stopwatch that returns HighResolutionTimeSpans
+    /// Gets 'Now' as CPU Time Stamp Counter (TSC)
     /// </summary>
-    public class HighResolutionStopWatch
+    public static long Now
     {
-        /// <summary>
-        /// Gets the start ticks
-        /// </summary>
-        public long StartTicks { get; private set; } = StopwatchHelper.CurrentTimeStamp;
-
-        /// <summary>
-        /// Gets the elapsed TimeSpan
-        /// </summary>
-        public HighResolutionTimeSpan Elapsed
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => new(this.StartTicks, StopwatchHelper.CurrentTimeStamp);
-        }
-
-        /// <summary>
-        /// Create a new stopwatch starting now
-        /// </summary>
-        /// <returns>new hs timespan</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static HighResolutionStopWatch StartNew()
-        {
-            return new HighResolutionStopWatch();
-        }
+        get => Stopwatch.GetTimestamp();
+    }
 
-        /// <summary>
-        /// reset the stopwatch
-        /// </summary>
+    /// <summary>
+    ///   Gets the initial date time
+    /// </summary>
+    public static DateTimeOffset InitialDateTime { get; } = DateTimeOffset.Now;
+
+    /// <summary>
+    ///  Gets the initial ticks TimeStamp
+    /// </summary>
+    public static long InitialTimeStamp { get; } = Now;
+
+    /// <summary>
+    /// Gets the number of ticks/second
+    /// </summary>
+    public static long TicksPerSecond
+    {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Reset()
+        get;
+    }
+
+= Stopwatch.Frequency;
+
+    /// <summary>
+    /// Gets the number of seconds/tick
+    /// </summary>
+    public static double SecondsPerTick
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+    }
+
+= 1d / TicksPerSecond;
+
+    /// <summary>
+    /// Gets the number of Milliseconds/tick
+    /// </summary>
+    public static double MillisecondsPerTick
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+    }
+
+= PerSecond.Milliseconds / TicksPerSecond;
+
+    /// <summary>
+    /// Gets the number of microseconds/tick
+    /// </summary>
+    public static double MicrosecondsPerTick
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+    }
+
+= PerSecond.Microseconds / TicksPerSecond;
+
+    /// <summary>
+    /// Gets the number of Nanoseconds/tick
+    /// </summary>
+    public static double NanosecondsPerTick
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+    }
+
+= PerSecond.Nanoseconds / TicksPerSecond;
+
+    /// <summary>
+    /// Gets the start timestamp
+    /// </summary>
+    public long Start
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+    }
+
+    /// <summary>
+    /// Gets the Elapsed time
+    /// </summary>
+    /// <returns>timespan</returns>
+    public HighResolutionTimeSpan Elapsed
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new(this.Start, Now);
+    }
+
+    /// <summary>
+    /// Gets the Elapsed as a TimeSpan
+    /// </summary>
+    /// <returns>timespan</returns>
+    public TimeSpan ElapsedTimeSpan
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => AsTimeSpan(this.Start, Now);
+    }
+
+    /// <summary>
+    /// Converts timestamp to Seconds
+    /// </summary>
+    /// <param name="timestamp">timestamp</param>
+    /// <returns>seconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double ToSeconds(long timestamp)
+         => timestamp * SecondsPerTick;
+
+    /// <summary>
+    /// Converts timestamp to Milliseconds
+    /// </summary>
+    /// <param name="timestamp">timestamp</param>
+    /// <returns>Milliseconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double ToMilliseconds(long timestamp)
+         => timestamp * MillisecondsPerTick;
+
+    /// <summary>
+    /// Converts timestamp to Microseconds
+    /// </summary>
+    /// <param name="timestamp">timestamp</param>
+    /// <returns>Microseconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double ToMicroseconds(long timestamp)
+        => timestamp * MicrosecondsPerTick;
+
+    /// <summary>
+    /// Converts timestamp to Nanoseconds
+    /// </summary>
+    /// <param name="timestamp">timestamp</param>
+    /// <returns>Nanoseconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double ToNanoseconds(long timestamp)
+        => timestamp * NanosecondsPerTick;
+
+    /// <summary>
+    /// Converts timestamp to a TimeSpan
+    /// </summary>
+    /// <param name="timestamp">timestamp</param>
+    /// <returns>TimeSpan</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TimeSpan ToTimeSpan(long timestamp)
+        => TimeSpan.FromMilliseconds(ToMilliseconds(timestamp));
+
+    /// <summary>
+    /// Converts the ticks to a date time
+    /// </summary>
+    /// <param name="timestamp">ticks to convert</param>
+    /// <returns>datetime</returns>
+    public static DateTimeOffset ToDateTimeOffset(long timestamp)
+        => InitialDateTime + ToTimeSpan(timestamp);
+
+    /// <summary>
+    ///  Converts a TimeSpan to ticks
+    /// </summary>
+    /// <param name="timeSpan">timeSpan</param>
+    /// <returns>cpu ticks</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double ToTicks(TimeSpan timeSpan)
+        => timeSpan.TotalSeconds * TicksPerSecond;
+
+    /// <summary>
+    /// Computes the difference as Seconds
+    /// </summary>
+    /// <param name="start">start timestampe</param>
+    /// <param name="end">end timestamp</param>
+    /// <returns>Seconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double AsSeconds(long start, long end)
+        => ToSeconds(start - end);
+
+    /// <summary>
+    /// Computes the difference as Milliseconds
+    /// </summary>
+    /// <param name="start">start timestampe</param>
+    /// <param name="end">end timestamp</param>
+    /// <returns>Milliseconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double AsMilliseconds(long start, long end)
+        => ToMilliseconds(start - end);
+
+    /// <summary>
+    /// Computes the difference as Microseconds
+    /// </summary>
+    /// <param name="start">start timestampe</param>
+    /// <param name="end">end timestamp</param>
+    /// <returns>Microseconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double AsMicroseconds(long start, long end)
+        => ToMicroseconds(start - end);
+
+    /// <summary>
+    /// Computes the difference as Nanoseconds
+    /// </summary>
+    /// <param name="start">start timestampe</param>
+    /// <param name="end">end timestamp</param>
+    /// <returns>Nanoseconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double AsNanoseconds(long start, long end)
+        => ToNanoseconds(start - end);
+
+    /// <summary>
+    /// Computes the difference as seconds
+    /// </summary>
+    /// <param name="start">start timestampe</param>
+    /// <param name="end">end timestamp</param>
+    /// <returns>seconds</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TimeSpan AsTimeSpan(long start, long end)
+        => TimeSpan.FromMilliseconds(AsMilliseconds(start, end));
+
+    /// <summary>
+    /// Create a new stopwatch starting now
+    /// </summary>
+    /// <returns>new hs timespan</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static HighResolutionStopWatch StartNew()
+        => new(Now);
+
+    /// <summary>
+    /// Perform a SpinWait until the desired timestamp is reached
+    /// </summary>
+    /// <param name="timestamp">end timestamp</param>
+    /// <param name="iterations">used to spinwait</param>
+    /// <param name="ct">cancellation token</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void SpinUntil(long timestamp, sbyte iterations, CancellationToken ct)
+    {
+        while (Now < timestamp)
         {
-            this.StartTicks = StopwatchHelper.CurrentTimeStamp;
+            ct.ThrowIfCancellationRequested();
+            Thread.SpinWait(iterations);
         }
     }
+
+    /// <summary>
+    /// Adds a TimeSpan to a timestamp
+    /// </summary>
+    /// <param name="timestamp">intiial timestamp</param>
+    /// <param name="duration">timespan to add</param>
+    /// <returns>computed end timestamp</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long Add(long timestamp, TimeSpan duration)
+        => (long)(timestamp + ToTicks(duration));
+
+    /// <summary>
+    /// Computes a timestamp from now
+    /// </summary>
+    /// <param name="duration">timespan to add</param>
+    /// <returns>computed end timestamp</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long FromNow(TimeSpan duration)
+         => Add(Now, duration);
 }
--- Common/Abbotware.Core/Chrono/HighResolutionTimeSpan.cs
+++ Common/Abbotware.Core/Chrono/HighResolutionTimeSpan.cs
@@ -4,124 +4,82 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Abbotware.Core.Chrono
+namespace Abbotware.Core.Chrono;
+
+using System;
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// TimeSpan that uses the high resolution clock
+/// </summary>
+/// <param name="Start">start timestamp</param>
+/// <param name="End">end timestamp</param>
+public record struct HighResolutionTimeSpan(long Start, long End)
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Runtime.CompilerServices;
-    using Abbotware.Core.Extensions;
+    /// <summary>
+    /// Gets a value indicating whether the timer's expiration point has been reached, otherwise false
+    /// </summary>
+    public bool IsExpired
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => HighResolutionStopWatch.Now >= this.End;
+    }
 
     /// <summary>
-    /// TimeSpan that uses the high resolution clock
+    /// Gets the total Seconds
     /// </summary>
-    public struct HighResolutionTimeSpan : IEquatable<HighResolutionTimeSpan>
+    public double TotalSeconds
     {
-        /// <summary>
-        ///  Initializes a new instance of the <see cref="HighResolutionTimeSpan" /> struct.
-        /// </summary>
-        /// <param name="duration">The amount of time that can pass before the timer is considered expired</param>
-        public HighResolutionTimeSpan(TimeSpan duration)
-        {
-            var total_ticks = duration.Ticks * Stopwatch.Frequency / TimeSpan.TicksPerSecond;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => HighResolutionStopWatch.AsSeconds(this.Start, this.End);
+    }
 
-            this.Start = StopwatchHelper.CurrentTimeStamp;
-            this.End = this.Start + total_ticks;
-        }
+    /// <summary>
+    /// Gets the total Milliseconds
+    /// </summary>
+    public double TotalMilliseconds
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => HighResolutionStopWatch.AsMilliseconds(this.Start, this.End);
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="HighResolutionTimeSpan"/> struct.
-        /// </summary>
-        /// <param name="start">start ticks</param>
-        /// <param name="end">end ticks</param>
-        public HighResolutionTimeSpan(long start, long end)
-        {
-            this.Start = start;
-            this.End = end;
-        }
+    /// <summary>
+    /// Gets the total Microseconds
+    /// </summary>
+    public double TotalMicroseconds
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => HighResolutionStopWatch.AsMicroseconds(this.Start, this.End);
+    }
 
-        /// <summary>
-        /// Gets the start ticks
-        /// </summary>
-        public long Start { get; }
+    /// <summary>
+    /// Gets the total Nanoseconds
+    /// </summary>
+    public double TotalNanoseconds
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => HighResolutionStopWatch.AsNanoseconds(this.Start, this.End);
+    }
 
-        /// <summary>
-        /// Gets the end ticks
-        /// </summary>
-        public long End { get; }
+    /// <summary>
+    /// Gets the TimeSpan
+    /// </summary>
+    public TimeSpan Elapsed
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => HighResolutionStopWatch.AsTimeSpan(this.Start, this.End);
+    }
 
-        /// <summary>
-        ///     Gets a value indicating whether the timer's expiration point has been reached, otherwise false
-        /// </summary>
-        public bool IsExpired
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get { return StopwatchHelper.CurrentTimeStamp >= this.End; }
-        }
-
-        /// <summary>
-        /// equals operator
-        /// </summary>
-        /// <param name="left">left side</param>
-        /// <param name="right">right side</param>
-        /// <returns>true if equal</returns>
-        public static bool operator ==(HighResolutionTimeSpan left, HighResolutionTimeSpan right)
-        {
-            return left.Equals(right);
-        }
-
-        /// <summary>
-        /// not equals operator
-        /// </summary>
-        /// <param name="left">left side</param>
-        /// <param name="right">right side</param>
-        /// <returns>true if not equal</returns>
-        public static bool operator !=(HighResolutionTimeSpan left, HighResolutionTimeSpan right)
-        {
-            return !(left == right);
-        }
-
-        /// <inheritdoc/>
-        public override bool Equals(object? obj)
-        {
-            if (!this.StructPossiblyEquals<HighResolutionTimeSpan>(obj, out var other))
-            {
-                return false;
-            }
-
-#if NETSTANDARD2_0
-            if (other == null)
-            {
-                return false;
-            }
-#endif
-            return this.Equals(other);
-        }
-
-        /// <inheritdoc />
-        public bool Equals(HighResolutionTimeSpan other)
-        {
-            if (!this.Start.Equals(other.Start))
-            {
-                return false;
-            }
-
-            if (!EqualityComparer<long?>.Default.Equals(this.End, other.End))
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <inheritdoc/>
-        public override int GetHashCode()
-        {
-#if NETSTANDARD2_0
-            return (this.Start, this.End).GetHashCode();
-#else
-            return HashCode.Combine(this.Start, this.End);
-#endif
-        }
+    /// <summary>
+    /// Creates future expiring timepan - useful for low-latency timers
+    /// </summary>
+    /// <param name="duration">The amount of time that can pass before the timer is considered expired</param>
+    /// <returns>high-res time span</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static HighResolutionTimeSpan Future(TimeSpan duration)
+    {
+        var start = HighResolutionStopWatch.Now;
+        var end = HighResolutionStopWatch.Add(start, duration);
+        return new(start, end);
     }
 }
