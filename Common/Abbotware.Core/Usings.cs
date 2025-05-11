684a1e74820a60a43912fe604488e5ba0404667a
Anthony Abate <anthony.abate@gmail.com>
2025-05-11 11:52:09 -0400

    for new Threading.Lock

--- Common/Abbotware.Core/Usings.cs
+++ Common/Abbotware.Core/Usings.cs
@@ -3,5 +3,10 @@
 // Copyright © Abbotware, LLC 2012-2023. All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
-
+#if NET9_0_OR_GREATER
+global using Lock = System.Threading.Lock;
+#else
+global using Lock = System.Object;
+#endif
 global using Microsoft.Extensions.Logging;
+
